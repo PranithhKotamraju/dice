3faf914696562c7d4710e29f0ea667e57f02a24b
Sai Praneeth Krishna Kotamraju <saipraneethkrishnakotamraju@Sais-MacBook-Pro.local>
2021-05-04 13:00:55 -0400

    Inital Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +17,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    random();
+                  });
+                },
+                child: Image.asset('images/dice$leftDice.png')),
+          ),
+          Expanded(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                   random();
+                  });
+                },
+                child: Image.asset('images/dice$rightDice.png')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void random(){
+    rightDice = Random().nextInt(6) + 1;
+    leftDice = Random().nextInt(6) + 1;
   }
 }
